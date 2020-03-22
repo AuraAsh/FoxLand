21ecc904f2c173e30f0583ddc449cdc345e87420
Awraash <sanchez.aura8@gmail.com>
2020-03-22 02:12:04 -0500

    sroll

---
+++ Sidescroller 2D/Assets/Sidescroller2D/Scripts/Frog.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog : MonoBehaviour
+{
+    [SerializeField]private float rightCap;
+    [SerializeField]private float leftCap;
+
+    private bool facingLeft - true;
+
+
+}
--- Sidescroller 2D/Assets/Sidescroller2D/Scripts/PlayerController.cs
+++ Sidescroller 2D/Assets/Sidescroller2D/Scripts/PlayerController.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 public enum State
 {
-    idle, running, jumping, falling
+    idle, running, jumping, falling, hurt
 };
 public class PlayerController : MonoBehaviour
 {
+    //Start() Variables
     private Collider2D coll;
     private Rigidbody2D rb;
     private Animator Anim;
-
+   
+    //Inspector Variables
     [SerializeField] private State state = State.idle;
-    [SerializeField] private LayerMask ground;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpforce = 10f;
+    [SerializeField] private float hurtforce = 10f;
+    [SerializeField] private int Score = 0;
+    [SerializeField] private Text scoreText;
+
 
     private void Start()
     {
@@ -20,38 +28,106 @@ public class PlayerController : MonoBehaviour
     }
     private void Update()
     {
+        if(state != State.hurt)
+        {
+            Movement();
+        }
+        VelocityState();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Collectable")
+        {
+            scoreText.text = Score.ToString();
+            Score += 1;
+            Destroy(collision.gameObject); 
+        }
+    }
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            if (state == State.falling)
+            {
+                Jump();
+                Destroy(other.gameObject);  
+            }
+            else
+            {
+                state = State.hurt;
+                if (other.gameObject.transform.position.x > transform.position.x)
+                {
+                    rb.velocity = new Vector2(-hurtforce, rb.velocity.y);
+                }
+                else
+                {
+                    rb.velocity = new Vector2(hurtforce, rb.velocity.y);
+                }
+            }
+        }
+    }
+
+    private void Movement()
+    {
         #region SETTINGS MOVEMENT
         float hDirection = Input.GetAxis("Horizontal");
 
         if (hDirection < 0)
         {
-            rb.velocity = new Vector2(-5, rb.velocity.y);
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            state = State.running;
             transform.localScale = new Vector2(-1, 1);
+
+            Anim.SetBool("isRun", true);
         }
         else if (hDirection > 0)
         {
-            rb.velocity = new Vector2(5, rb.velocity.y);
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            state = State.running;
             transform.localScale = new Vector2(1, 1);
+
+            Anim.SetBool("isRun", true);
+        }
+        else
+        {
+            Anim.SetBool("isRun", false);
         }
         VelocityState();
 
         #endregion
-       
-        #region SETTINGS JUMP PLAYER
-        if (Input.GetButtonDown("Jump") && !coll.IsTouchingLayers(ground))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 10f);
-            state = State.jumping;
 
+        #region SETTINGS JUMP PLAYER
+        if (Input.GetButtonDown("Jump") && !coll.IsTouchingLayers())
+        {
+            Jump();
             Anim.SetBool("isJump", true);
         }
         else
         {
             Anim.SetBool("isJump", false);
-        } 
+        }
+        #endregion
+
+        #region SETTINGS HURT PLAYER
+        if (state == State.hurt)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, hurtforce);
+            Anim.SetBool("isHurt", true);
+            state = State.hurt;
+        }
+        else
+        {
+            Anim.SetBool("isHurt", false);
+        }
         #endregion
 
     }
+    public void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+        state = State.jumping;
+    }
+
     private void VelocityState()
     {
         if (state == State.jumping)
@@ -63,10 +139,21 @@ public class PlayerController : MonoBehaviour
         }
         else if (state == State.falling)
         {
-            if (coll.IsTouchingLayers(ground))
+            if (coll.IsTouchingLayers())
             {
                 state = State.idle;
             }
+        }
+        else if (state == State.hurt)
+        {
+            if (Mathf.Abs(rb.velocity.x) < .1f)
+            {
+                state = State.idle;
+            }
+            else
+            {
+                Anim.SetBool("isHurt", true);
+            } 
         }
         else if (Mathf.Abs(rb.velocity.x) > 2f)
         {
